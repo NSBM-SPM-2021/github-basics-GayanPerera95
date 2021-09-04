08bca157cbc681dfa0465dd5bd59b8e3257228ec
GayanPerera95 <gayanmadushanka456@gmail.com>
2021-09-04 20:35:39 +0530

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen, cleanup } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByTestId('todo-1');
-  expect(linkElement).toBeInTheDocument();
-});
